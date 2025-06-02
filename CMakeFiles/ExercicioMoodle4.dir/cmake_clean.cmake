d2cf1a7ae92f0df9f482c37a1e72b9805fead389
Leonardo Eugenio <leonardoeugenio977@gmail.com>
2025-06-02 20:50:03 -0300

    Jogo das cores

---
+++ CMakeFiles/ExercicioMoodle4.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExercicioMoodle4.dir/common/glad.c.obj"
+  "CMakeFiles/ExercicioMoodle4.dir/common/glad.c.obj.d"
+  "CMakeFiles/ExercicioMoodle4.dir/src/Modulo2/ExercicioMoodle4.cpp.obj"
+  "CMakeFiles/ExercicioMoodle4.dir/src/Modulo2/ExercicioMoodle4.cpp.obj.d"
+  "ExercicioMoodle4.exe"
+  "ExercicioMoodle4.exe.manifest"
+  "ExercicioMoodle4.pdb"
+  "libExercicioMoodle4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ExercicioMoodle4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
